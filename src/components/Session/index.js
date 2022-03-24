dcf80ae98c0e11e6a7352b513f22745a3ece9784
Icaro-pavani <icaro.pavani79@gmail.com>
2022-03-24 01:18:44 -0300

    Corrections to the code

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 function Session(props) {
-    const {id, date, showtimes} = props;
+    const {date, showtimes} = props;
 
     return (
         <li className="Session">
             <h3>{date}</h3>
             <div className="session-times">
-                {showtimes.map(({id, name}) => <Link to={`/sessao/${id}`}><button key={id}>{name}</button></Link>)}
+                {showtimes.map(({id, name}) => <Link to={`/sessao/${id}`} key={id}><button>{name}</button></Link>)}
             </div>
         </li>
     );
