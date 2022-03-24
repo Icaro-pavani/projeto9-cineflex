764d43555ca948e96b9f9778055b414490992331
Icaro-pavani <icaro.pavani79@gmail.com>
2022-03-24 02:40:52 -0300

    Seats layout page built

--- src/components/Seats/index.js
+++ src/components/Seats/index.js
@@ -25,7 +25,28 @@ function Seats() {
         <div className="Seats">
             <main>
                 <h2>Selecione o(s) assento(s)</h2>
-                {seatsMap.map(seatsRow => <SeatsRow key={seatsRow} seatsRow={seatsRow}/>)}            
+                {seatsMap.map(seatsRow => <SeatsRow key={seatsRow} seatsRow={seatsRow}/>)}
+                <div className="seat-info">
+                    <div className="example-circle">
+                        <div className="circle selected"></div>
+                        <p>Selecionado</p>
+                    </div>
+                    <div className="example-circle">
+                        <div className="circle available"></div>
+                        <p>Disponível</p>
+                    </div>
+                    <div className="example-circle">
+                        <div className="circle unavailable"></div>
+                        <p>Indisponível</p>
+                    </div>
+                </div> 
+                <form>
+                    <h3>Nome do comprador:</h3>
+                    <input name="nome" type="text" placeholder="Digite seu nome..." />
+                    <h3>CPF do comprador:</h3>
+                    <input name="CPF" type="text" placeholder="Digite seu CPF..." />
+                    <input className="submit" type="submit" value="Reservar assento(s)" />
+                </form>           
             </main>
             <Footer image={"teste"} title={"tres"} session="" />
         </div>
